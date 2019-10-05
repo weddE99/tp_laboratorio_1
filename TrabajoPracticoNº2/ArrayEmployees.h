e30d5e0e48542f70f83ff354bbcbd2ddfa2a1d69
weddE99 <54249487+weddE99@users.noreply.github.com>
2019-10-04 23:06:01 -0300

    Add files via upload

--- TrabajoPracticoNº2/ArrayEmployees.h
+++ TrabajoPracticoNº2/ArrayEmployees.h
@@ -7,18 +7,17 @@ struct{
     int isEmpty;
 }typedef Employee;
 
-/** \brief  To indicate that all position in the array are empty,
-*           this function put the flag (isEmpty) in TRUE in all
-*           position of the array
-* \param list Employee* Pointer to array of employees
+/** \brief  Para indicar que todas las posiciones del array estan vacios.
+*
+* \param list Employee* puntero al array de Employee
 * \param len int Array length
-* \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
+* \return int Return (0)
 *
 */
 int initEmployees(Employee* list, int len);
 
-/** \brief add in a existing list of employees the values received as parameters
-*     in the first empty position
+/** \brief agregar en una lista existente de empleados los valores recibidos como parámetros
+*          en la primera posición vacía que se encuentre.
 * \param list employee*
 * \param len int
 * \param id int
@@ -26,12 +25,55 @@ int initEmployees(Employee* list, int len);
 * \param lastName[] char
 * \param salary float
 * \param sector int
-* \return int Return (-1) if Error [Invalid length or NULL pointer or without free space] - (0) if Ok
+* \return int Return (0)
 *
 */
 int addEmployee(Employee* list, int len, char name[], char lastName[], float salary, int sector);
+
+/** \brief encuentra un empleado en Employee por la id, retorna la posicion del array del id.
+*
+* \param list Employee*
+* \param len int
+* \param id int
+* \return int Return (id) si esta todo bien, (-1) si hay algun error.
+*
+*/
 int findEmployeeById(Employee* list, int len,int id);
+
+/** \brief Elimina un empleado por id, (setea isEmpty en 0).
+*
+* \param list Employee*
+* \param len int
+* \param id int
+* \return int Return (-1) si no encuenta el id, (0) si esta ok.
+*
+*/
 int removeEmployee(Employee* list, int len, int id);
+
+/** \brief ordena los elementos del array Employee, el argumento indica si ascendente o descendente
+*
+* \param list Employee*
+* \param len int
+* \param order int [1] indicando ascendente - [2] indicando descendente
+* \return int Return (0)
+*
+*/
 int sortEmployees(Employee* list, int len, int order);
+
+/** \brief imprime el contenido del array de Employee
+*
+* \param list Employee*
+* \param length int
+* \return int Return (0)
+*
+*/
 int printEmployees(Employee* list, int length);
+
+/** \brief menu que se inicia al comenzar el programa, contiene las opciones de ingreso
+*
+* \param list Employee*
+* \param length int
+* \return int Return (0)
+*
+*/
 void menu(Employee* list, int length);
